647ccfc1ef97827b6703687324f2194043569842
Fann Wu <fann.wu@pathfinders.studio>
2023-02-22 17:37:52 +0800

    Fixed cros setting.

--- go.mod
+++ go.mod
@@ -12,6 +12,24 @@ require (
 	github.com/btcsuite/btcd v0.21.0-beta // indirect
 	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
 	github.com/ethereum/go-ethereum v1.10.4 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210420205809-ac73e9fd8988 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -22,6 +22,22 @@ type request struct {
 	Resp chan randResponse
 }
 
+type cryptoKeonRequest struct {
+    SelectedFields	  []int      `json:"selectedField"`
+    BetAmount         float64    `json:"betAmount"`
+}
+
+type cryptoKeonResponse struct {
+    Payout     string    `json:"Payout"`  
+    WinFields  []int     `json:"WinFields"`
+    Profit     float64   `json:"Profit"`
+}
+
+type keonRequest struct {
+    Req cryptoKeonRequest
+    Resp chan cryptoKeonResponse
+}
+
 const (
 	maxWorkers = 3
 	maxQueue   = 5
@@ -33,7 +49,8 @@ func main() {
 		activeTask int64
 	)
 
-	taskQueue := make(chan request, maxQueue)
+	taskQueue  := make(chan request, maxQueue)
+    taskQueue2 := make(chan keonRequest, maxQueue) 
 
 	// create workers
 	for i := 0; i < maxWorkers; i++ {
@@ -47,16 +64,34 @@ func main() {
 				atomic.AddInt64(&activeTask, -1)
 			}
 		}()
+
+        go func() {
+			for req := range taskQueue2 {
+				resp := cryptoKeonResponse{
+					Payout: "0.000x",
+                    WinFields: []int{39,24,37,34,6,2,11,18,32,19},
+                    Profit: -0.00000100,
+				}
+				req.Resp <- resp
+				atomic.AddInt64(&queueSize, -1)
+				atomic.AddInt64(&activeTask, -1)
+			}
+		}()
 	}
 
 	// Set up the http handler function
 	http.HandleFunc("/testrand", handleRequest(&queueSize, &activeTask, taskQueue))
+    http.HandleFunc("/cryptokeon", cryptoKeon(&queueSize, &activeTask, taskQueue2))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":5566", nil))
 }
 
 func handleRequest(queueSize *int64, activeTask *int64, taskQueue chan request) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 		if r.Method != "POST" {
 			http.Error(w, "Only POST requests are supported", http.StatusMethodNotAllowed)
 			return
@@ -113,3 +148,65 @@ func handleRequest(queueSize *int64, activeTask *int64, taskQueue chan request) 
 	}
 }
 
+func cryptoKeon(queueSize *int64, activeTask *int64, taskQueue chan keonRequest) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "http://192.168.0.133:8080")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        
+        if r.Method != "POST" {
+			http.Error(w, "Only POST requests are supported", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Limit the number of requests to maxQueue
+		if atomic.LoadInt64(queueSize) >= maxQueue {
+			http.Error(w, "Too many requests queued", http.StatusServiceUnavailable)
+			fmt.Println("queueSize:", atomic.LoadInt64(queueSize), "activeTask:", atomic.LoadInt64(activeTask))
+			return
+		}
+		atomic.AddInt64(queueSize, 1)
+
+		// Decode the request
+		var cryptokeonReq cryptoKeonRequest
+		err := json.NewDecoder(r.Body).Decode(&cryptokeonReq)
+		if err != nil {
+			http.Error(w, "Invalid request format", http.StatusBadRequest)
+			return
+		}
+
+		// Create a channel for the response
+		respChan := make(chan cryptoKeonResponse)
+
+		// Wrap the request in a task function and send it to the task queue
+		req := keonRequest{
+			Req:  cryptokeonReq,
+			Resp: respChan,
+		}
+
+		// Check if there are too many active tasks
+		if atomic.LoadInt64(activeTask) >= maxWorkers {
+			http.Error(w, "Too many active tasks", http.StatusServiceUnavailable)
+			atomic.AddInt64(queueSize, -1)
+			return
+		}
+
+		taskQueue <- req
+		atomic.AddInt64(activeTask, 1)
+
+		// Wait for the response
+		select {
+		case resp := <-respChan:
+			close(respChan)
+			err := json.NewEncoder(w).Encode(resp)
+			if err != nil {
+				log.Println("Failed to write response:", err)
+			}
+		case <-time.After(10 * time.Second):
+			http.Error(w, "Request timed out", http.StatusRequestTimeout)
+			atomic.AddInt64(queueSize, -1)
+			atomic.AddInt64(activeTask, -1)
+		}
+	}
+}
